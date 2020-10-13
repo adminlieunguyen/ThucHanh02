8386410066b5784ff8bec19e53e2686c086645d6
lieu1489 <lieuutehy21@gmail.com>
2020-10-14 01:07:16 +0700

    update by lieu10117330

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './bai1/header/header.component';
+import { BodyComponent } from './bai1/body/body.component';
+import { FooterComponent } from './bai1/footer/footer.component';
+import { Header2Component } from './bai2/header2/header2.component';
+import { Body2Component } from './bai2/body2/body2.component';
+import { Footer2Component } from './bai2/footer2/footer2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent,
+    Header2Component,
+    Body2Component,
+    Footer2Component,
   ],
   imports: [
     BrowserModule,
